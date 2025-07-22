ca2c70ff461c52a7cd38071bb615cbe20a40cbf5
VISHWAr23 <vishwarajkumar05@gmail.com>
2025-07-22 23:58:36 +0530

    changes made fordeploy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3003);
+  await app.listen(3000, '0.0.0.0');
+  console.log('Application is running on: http://localhost:3000');
 }
+
 bootstrap();
